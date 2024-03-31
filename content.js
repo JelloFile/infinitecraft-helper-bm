79060fd09e1f2e861d2ba3e6528251cc87ead0ca
JelloFile <123781425+JelloFile@users.noreply.github.com>
2024-03-31 17:40:05 -0400

    Create content.js

---
+++ content.js
@@ -0,0 +1,3 @@
+var sidebar_sorting = document.querySelector(".sidebar-sorting");
+
+sidebar_sorting.innerHTML = sidebar_sorting.innerHTML + '<div data-v-2f8bc7f9="" class="sidebar-sorting-item"><img data-v-2f8bc7f9="" src="/infinite-craft/magic.svg" class="sidebar-sorting-icon">Upload Save File</div>'

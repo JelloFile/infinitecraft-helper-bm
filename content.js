e378bd0b8447592e436819a12344b2d4b8e3ca8b
JelloFile <123781425+JelloFile@users.noreply.github.com>
2024-03-31 18:26:46 -0400

    Update content.js

--- content.js
+++ content.js
@@ -29,7 +29,7 @@ function handleFileSelect(){
       const contents = reader.result;
       console.log(contents);
       localStorage.setItem("infinite-craft-data", contents);
-     location.reload;
+     location.reload();
     };
    reader.readAsText(file);
   }</script><style>html,body{text-align:center;}</style>`
